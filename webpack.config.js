20ad24496758142daff98f9e8a0df838a9b399fb
Guy Balaam <guybal@amazon.com>
2019-01-19 23:35:09 +0000

    Adding scss

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,23 @@
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     mode: 'production',
     entry: ['./src/index'],
 
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                'css-loader',
+                'sass-loader'
+            ]
+        }]
+    },
+
     plugins: [
-        new CleanWebpackPlugin(['dist'])
+        new CleanWebpackPlugin(['dist']),
+        new MiniCssExtractPlugin({}),
     ]
 }
